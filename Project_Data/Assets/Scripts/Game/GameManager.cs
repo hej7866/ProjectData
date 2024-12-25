de353f7501c115c63c0d91bd77633c91605c83cb
hej7866 <hej7866@naver.com>
2024-12-26 03:37:21 +0900

    (2024.12.24) UI 아이템 슬롯 구현

--- Project_Data/Assets/Scripts/Game/GameManager.cs
+++ Project_Data/Assets/Scripts/Game/GameManager.cs
@@ -2,14 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : SingleTon<GameManager>
 {
-
-    public static GameManager Instance;
     public GameData gameData;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
 }
